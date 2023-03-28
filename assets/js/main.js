18b934f2714bddcefa214e09e23af351e88e90cd
TomiiVaz <eliseovazquez01@gmail.com>
2023-03-28 20:00:38 -0300

    Migrando - Vue v1.6

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,39 +5,22 @@ const { createApp } = Vue
 const appCat = createApp({
     data() {
         return {
-            categories: []
+            cards: [],
+            categories: [],
+            search: "",
         }
     },
-    mounted() {
+    created() {
         this.getCategories()
-        addEventListener('change', () => {
-            console.log("ADIOS")
-        })
-    },
-    methods: {
-        getCategories() {
-            dataApi.events.forEach((event) => {
-                if (!this.categories.includes(event.category)) {
-                    this.categories.push(event.category);
-                }
-            });
-        }
-    }
-
-}).mount('#categories')
-
-const appCard = createApp({
-    data() {
-        return {
-            cards: []
-        }
+        this.getCards(dataApi.events)
     },
     mounted() {
-        this.getCards(dataApi)
+        this.search = document.getElementById('search')
     },
     methods: {
         getCards(info) {
-            dataApi.events.forEach((event) => {
+            this.cards = []
+            info.forEach((event) => {
                 this.cards.push({
                     id: event._id,
                     name: event.name,
@@ -47,59 +30,41 @@ const appCard = createApp({
                 })
             })
         },
-    }
 
-}).mount('#content')
-
-const appFilter = createApp({
-    data() {
-        return {
-            cards: []
-        }
-    },
-    mounted() {
-        addEventListener('change', () => {
-            console.log("ADIOS")
-        })
-    },
-    methods: {
         getCategories() {
             dataApi.events.forEach((event) => {
                 if (!this.categories.includes(event.category)) {
                     this.categories.push(event.category);
                 }
             });
-        }
+        },
+
+        getGlobalFilter() {
+            let searchFilter = this.getFilterCardSearch(dataApi, this.search)
+            let checkOfSearchFilter = this.getFilterCardCheckbox(searchFilter)
+            this.getCards(checkOfSearchFilter)
+        },
+
+        getFilterCardCheckbox(data) {
+            let checkboxes = document.querySelectorAll("input[type='checkbox']")
+            let arraychecks = Array.from(checkboxes)
+            let checksChecked = arraychecks.filter(check => check.checked)
+            if (checksChecked.length == 0) {
+                return data
+            }
+            let checkValues = checksChecked.map(check => check.value)
+            let arrayFiltrado = data.filter(elemento => checkValues.includes(elemento.category))
+            return arrayFiltrado
+        },
+
+        getFilterCardSearch(data, search) {
+            let searchValue = search.value
+            let eventsFiltered = data.events.filter(event => event.name.toLowerCase().includes(searchValue.toLowerCase()))
+            return eventsFiltered
+        },
+    },
+    computed: {
+        // Pasar los filtros acá
     }
 
-}).mount('#filter')
-
-// const contCategories = document.getElementById('categories');
-// let search = document.getElementById('search')
-
-
-// function getGlobalFilter() {
-//     let searchFilter = getFilterCardSearch(dataApi, search)
-//     let checkOfSearchFilter = getFilterCardCheckbox(searchFilter)
-//     getCards(checkOfSearchFilter)
-// }
-
-// function getFilterCardSearch(data, search) {
-//     let searchValue = search.value
-//     let eventsFiltered = data.events.filter(event => event.name.toLowerCase().startsWith(searchValue.toLowerCase()))
-//     return eventsFiltered
-// }
-// function getFilterCardCheckbox(data) {
-//     let checkboxes = document.querySelectorAll("input[type='checkbox']")
-//     let array    checks = Array.from(checkboxes)
-//     let checksChecked = arraychecks.filter(check => check.checked)
-//     if (checksChecked.length == 0) {
-//         return data
-//     }
-//     let checkValues = checksChecked.map(check => check.value)
-//     let arrayFiltrado = data.filter(elemento => checkValues.includes(elemento.category))
-//     return arrayFiltrado
-// }
-
-// contCategories.addEventListener('change', getGlobalFilter)
-// search.addEventListener("input", getGlobalFilter)
+}).mount('#app')
--- assets/js/pevents.js
+++ assets/js/pevents.js
@@ -1,53 +1,75 @@
-import { getCategories, getCards, dataApi } from "./data.js"
-// Imprime todas las categorias en check
-getCategories(dataApi)
+import { dataApi } from './data.js';
 
-// Es el contenedor de los check
-const contCategories = document.getElementById('categories');
-// Es el input
-let searchValue = document.getElementById('search')
-// Es el contenedor de cada tarjeta
-const content = document.getElementById('content')
-const currentDate = dataApi.currentDate
-let pastEvents = []
+const { createApp } = Vue
 
-for (let event of dataApi.events) {
-    if (event.date < currentDate) {
-        pastEvents.push(event)
-        continue
+const appCat = createApp({
+    data() {
+        return {
+            cards: [],
+            categories: [],
+            search: "",
+        }
+    },
+    created() {
+        this.getCategories()
+        this.getCards(dataApi.events)
+    },
+    mounted() {
+        this.search = document.getElementById('search')
+    },
+    methods: {
+        getCards(info) {
+            this.cards = []
+            info.forEach((event) => {
+                if (event.date < dataApi.currentDate) {
+                    this.cards.push({
+                        id: event._id,
+                        name: event.name,
+                        description: event.description,
+                        image: event.image,
+                        price: event.price,
+                    })
+                }
+
+            })
+        },
+
+        getCategories() {
+            dataApi.events.forEach((event) => {
+                if (event.date < dataApi.currentDate) {
+                    if (!this.categories.includes(event.category)) {
+                        this.categories.push(event.category);
+                    }
+                }
+            });
+        },
+
+        getGlobalFilter() {
+            let searchFilter = this.getFilterCardSearch(dataApi, this.search)
+            let checkOfSearchFilter = this.getFilterCardCheckbox(searchFilter)
+            this.getCards(checkOfSearchFilter)
+        },
+
+        getFilterCardCheckbox(data) {
+            let checkboxes = document.querySelectorAll("input[type='checkbox']")
+            let arraychecks = Array.from(checkboxes)
+            let checksChecked = arraychecks.filter(check => check.checked)
+            if (checksChecked.length == 0) {
+                return data
+            }
+            let checkValues = checksChecked.map(check => check.value)
+            let arrayFiltrado = data.filter(elemento => checkValues.includes(elemento.category))
+            return arrayFiltrado
+        },
+
+        getFilterCardSearch(data, search) {
+            let searchValue = search.value
+            let eventsFiltered = data.events.filter(event => event.name.toLowerCase().includes(searchValue.toLowerCase()))
+            return eventsFiltered
+        },
+    },
+    computed: {
+        // Pasar los filtros acá
     }
-}
 
-// Imprime todas las tarjetas correspondientes
-getCards(pastEvents)
-
-// Filtra por el input search y retorna filtrado
-function getFilterCardSearch(data, search) {
-    let searchValue = search.value
-    let eventsFiltered = data.events.filter(event => event.name.toLowerCase().startsWith(searchValue.toLowerCase()))
-    return eventsFiltered
-}
-
-function getFilterCardCheckbox(data) {
-    let checkboxes = document.querySelectorAll("input[type='checkbox']")
-    let arraychecks = Array.from(checkboxes)
-    let checksChecked = arraychecks.filter(check => check.checked)
-    if(checksChecked.length == 0){
-        return data
-    }
-    let checkValues = checksChecked.map(check => check.value)
-    let arrayFiltrado = data.filter(elemento => checkValues.includes(elemento.category))
-    return arrayFiltrado
-}
-
-function getGlobalFilter(){
-    let searchFilter = getFilterCardSearch(dataApi, search)
-    let checkOfSearchFilter = getFilterCardCheckbox(searchFilter)
-    getCards(checkOfSearchFilter)
-}
-
-// Escucha cada tecla del search
-search.addEventListener("input", getGlobalFilter)
-
-// Escucha cada cambio en los check filtra e imprime
-contCategories.addEventListener('change', getGlobalFilter)
+}).mount('#app')
